a8392b02b85b73d109e595376eca1dd413f057d8
cristian.costantini <cristian@costantini.me>
2026-06-23 14:57:49 +0200

    Revert "hive lib"
    This reverts commit f854ff2fefad1303b0c325da13a5fd2e57fbd386.

--- hive-archunit/src/test/java/io/sinapsi/hive/archunit/fixtures/compliant/application/ports/out/StorePort.java
+++
@@ -1,7 +0,0 @@
-package io.sinapsi.hive.archunit.fixtures.compliant.application.ports.out;
-
-import io.sinapsi.hive.archunit.fixtures.compliant.domain.Amount;
-
-public interface StorePort {
-    void store(Amount amount);
-}
